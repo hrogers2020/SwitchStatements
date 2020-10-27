b1ae37b8819e2978339c2420cf7edb6289a281ee
Hampton Rogers <rogershampton22@gmail.com>
2020-10-27 15:46:10 -0500

    C# is the best language

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,23 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string subject = "C#";
+
+            Console.WriteLine("Choose a subject");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "C#":
+                    Console.WriteLine("You are in the right class!");
+                    break;
+                case "Java":
+                    Console.WriteLine("C# is better than Java");
+                    break;
+                default:
+                    Console.WriteLine("That subject sucks, we are in the C# class");
+                    break;
+            }
         }
     }
 }
